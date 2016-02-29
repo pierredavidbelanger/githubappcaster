cef7836b1dd46157cd5381358243b4f1bb8d7677
Pierre-David Bélanger <pierredavidbelanger@gmail.com>
2016-02-29 17:31:51 -0500

    make JAXB use hardcoded namespace prefixes (https://github.com/sparkle-project/Sparkle/issues/771)

---
+++ src/main/java/ca/pjer/githubappcaster/package-info.java
@@ -0,0 +1,8 @@
+@XmlSchema(xmlns = {
+        @XmlNs(prefix = "sparkle", namespaceURI = "http://www.andymatuschak.org/xml-namespaces/sparkle"),
+        @XmlNs(prefix = "dc", namespaceURI = "http://purl.org/dc/elements/1.1/")
+})
+package ca.pjer.githubappcaster;
+
+import javax.xml.bind.annotation.XmlNs;
+import javax.xml.bind.annotation.XmlSchema;
